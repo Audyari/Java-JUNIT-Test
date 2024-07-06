b77cd49932dc77841b8b48430a903c168dd58374
Audyari Wiyono <audy123ari@gmail.com>
2024-07-06 19:03:37 +0700

    Pengenalan JUnit

---
+++ src/main/java/Programmer/testing/App.java
@@ -0,0 +1,15 @@
+package Programmer.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+
+
+    {
+        System.out.println( "Hello World!" );
+    }
+}
